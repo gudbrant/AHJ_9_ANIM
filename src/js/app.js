68d776a38bee24f6c1e3ee709eab84f7ae7211fb
gudbrant <gudbrant@gmail.com>
2021-03-23 10:41:00 +0800

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import Collapse from './collapse.js';
+import Chat from './chat.js';
+import Liker from './liker.js';
+
+const collapse = new Collapse();
+collapse.init();
+
+const chat = new Chat();
+chat.init();
+
+const liker = new Liker();
+liker.init();
